3665d5f45ec9a586cb7c17a3b05750abd3166c3e
Steven Pennington <spennington45@gmail.com>
2020-09-02 12:39:38 -0400

    merge conflict

--- 09-Capstone/student/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
+++ 09-Capstone/student/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -3,37 +3,28 @@ package com.techelevator.tenmo.model;
 import java.math.BigDecimal;
 
 public class Account {
-	private BigDecimal balance;
+
 	private int accountId;
 	private int userId;
-	
-	public Account() {
-	}
-	
-	public BigDecimal getBalance(int userId) {
-		return balance;
-	}
-	
-	public void setBalance(BigDecimal balance) {
-		this.balance = balance;
-	}
+	private BigDecimal balance;
 	
 	public int getAccountId() {
 		return accountId;
 	}
-
 	public void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
-
 	public int getUserId() {
 		return userId;
 	}
-
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-
-
-
+	public BigDecimal getBalance() {
+		return balance;
+	}
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+	
 }
